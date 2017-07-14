3fa009e34336c2bc6eea93b48ba8b03341b1751a
prashanthanem <prashanthanem@gmail.com>
2017-07-14 10:28:17 +0530

    DB file changed according to heroku

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = @mysql_connect('localhost', 'root',"kedbdb");
+$connection = @mysql_connect('us-cdbr-iron-east-03.cleardb.net', 'b28e25d5c88e60',"9a07b9bf");
 if (!$connection){
     die("Database Connection Failed" . @mysql_error($connection));
 }
-$select_db = @mysql_select_db('kedb',$connection);
+$select_db = @mysql_select_db('heroku_e8fbe5e3a9873d3',$connection);
 if (!$select_db){
     die("Database Selection Failed" . @mysql_error($connection));
 }
